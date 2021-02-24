e1995dd3bc70e2071f968db72ccd9f2dc75cacf9
sawasawasawa <m.e.sawka@gmail.com>
2021-02-24 14:38:44 +0100

    copy localstorage with cookies

--- cookie-cutter.js
+++ cookie-cutter.js
@@ -1,9 +1,16 @@
 // Local storage Key ID.
 const COOKIE_JAR_ID = 'bf2b15a7912-cookie-cutter-cookie-jar';
+const LOCALSTORAGE_JAR_ID = 'bf2b15a7912-cookie-cutter-localstorage-jar';
 
 // Script to run on the page to retrieve the cookie.
 const getCookieFn = () => {
   return '(() => { return document.cookie; })();';
+};
+
+const getLocalStorageFn = () => {
+  return `(() => { 
+  return JSON.stringify({...localStorage}); })();
+  `;
 };
 
 // Script to run to set the cookie for the page.
@@ -26,6 +33,10 @@ const setCookieFn = (cookieString) => {
   return `(() => { ${result} })();`;
 };
 
+const setLocalStorageFn = (localStorageString) => {
+  return `(() => { Object.entries(${localStorageString}).forEach(([key, value])=>localStorage.setItem(key, value)) })();`;
+};
+
 const cookie2Json = (cookieString) => {
   return cookieString.split(';').reduce((hash, pair) => {
     const [key, value] = pair.split('=');
@@ -42,17 +53,24 @@ const copyCookie = () => {
     chrome.tabs.executeScript(tab.id, { code: getCookieFn() }, (cookieString) => {
       window.localStorage.setItem(COOKIE_JAR_ID, cookieString);
     });
+
+    chrome.tabs.executeScript(tab.id, { code: getLocalStorageFn() }, (localStorageString) => {
+      window.localStorage.setItem(LOCALSTORAGE_JAR_ID, localStorageString);
+    });
   });
 
 };
 
 const pasteCookie = () => {
   const cookieString = window.localStorage.getItem(COOKIE_JAR_ID);
+  const localStorageObject = window.localStorage.getItem(LOCALSTORAGE_JAR_ID);
+
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     const tab = tabs[0];
     chrome.tabs.executeScript(tab.id, { code: setCookieFn(cookieString) });
-  });
-};
+
+    chrome.tabs.executeScript(tab.id, { code: setLocalStorageFn(localStorageObject) });
+})};
 
 // Chrome setup
 const contextMenuIds = {
